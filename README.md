f59ccefc502a38912959076bd7a913f9fafdc2e3
ratanparai <ratanparai@gmail.com>
2013-11-29 20:01:41 +0600

    Create Project and edit readme (+How to run)

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-Learn_Java
+Learn Java
 ==========
 
-Java sample programs
+Java sample programs Solution from text book
+
+
+How to run?
+===========
+
+Please opne the file you want to run and then Run>Run file (Shift+F6).
---
+++ varsity/src/varsity/Varsity.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package varsity;
+
+/**
+ *
+ * @author Ratan
+ */
+public class Varsity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
